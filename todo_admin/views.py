e8f824f8eaa79abed6977d0c0af97dc7106d06d4
Shrut jain <shrutjain432@gmail.com>
2025-11-12 23:04:12 +0530

    todo

--- todo_admin/views.py
+++ todo_admin/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     tasks = Task.objects.filter(is_completed = False).order_by('update_at') # It will fetch task that are not completed
+    completed_tasks = Task.objects.filter(is_completed = True) # It will have task that are tham completed
     context = {
         'tasks' : tasks,
+        'completed_tasks' : completed_tasks
     }
-    
     return render(request, 'home.html')
